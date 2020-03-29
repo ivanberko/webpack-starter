b039e3eb3cfe1ece28e531520f04a4cab1f76a20
Ivan Berk <berkoivan@gmail.com>
2020-03-29 21:55:18 +0300

    added pnotify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './style.css';
 import axios from 'axios';
 import { v4 } from 'uuid';
+import debounce from 'lodash.debounce';
+import PNotify from 'pnotify/dist/es/PNotify.js';
+import PNotifyButtons from 'pnotify/dist/es/PNotifyButtons.js';
+import 'pnotify/dist/PNotifyBrightTheme.css';
+
